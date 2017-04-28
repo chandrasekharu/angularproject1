6e9a8c53ccdca4032edd4d633c76e5d302cdb892
chandrasekharu <ucsraju@gmail.com>
2017-04-27 22:37:20 -0500

    need to work user input

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+import { Hero } from './hero';
+@Component({
+    selector: 'app-root',
+    templateUrl:'app.component.html'
+})
+export class AppComponent {
+    title ='Tour of Heroes';
+    heroes = [
+        new Hero(1, 'Windstorm'),
+        new Hero(13, 'Bombasto'),
+        new Hero(15, 'Magneta'),
+    ];
+    myHero = this.heroes[0];
+}
---
+++ my-app/src/app/hero.ts
@@ -0,0 +1,6 @@
+﻿export class Hero {
+    constructor(public id: number,
+        public name: string) {
+
+    }
+}
